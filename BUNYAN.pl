f52b5602d82f3b48414d44c0ddb2756ce8d98b9a
Prince of Archives <rich.tijerina@gmail.com>
2017-03-30 18:31:34 -0600

    Create BUNYAN.pl

---
+++ BUNYAN.pl
@@ -0,0 +1,7 @@
+########################
+# BUNYAN - MINION OUTPUT
+"$MINION $iteration $size(optional)\n"
+@ARGV[0] = print delay modifier
+$MINION = normal ascii purple
+$iteration = bold ascii yellow
+$size = GB-red MB-green KB-white
