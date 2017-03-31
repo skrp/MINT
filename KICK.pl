dc2df40c8d322eac89a8331f9463453e672f65e5
Prince of Archives <rich.tijerina@gmail.com>
2017-03-30 18:39:16 -0600

    Create KICK.pl

---
+++ KICK.pl
@@ -0,0 +1,12 @@
+#!/usr/local/bin/perl
+use strict; use warnings;
+use Term::ANSIColor;
+##################
+# KICK - print pid
+my (@active) = @ARGV;
+foreach $minion (@active) {
+  my $file = "$minion/$minion".'_PID';
+  open(my $pfh, '<', $file);
+  my $content = readline $pfh; chomp $content; close $pfh;
+  print color('magenta'); print "$minion "; print color('bold yellow'); print "$_\n";
+}
