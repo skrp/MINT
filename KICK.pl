929a66dee2faddc0b71a92d75b227b56869693ab
Prince of Archives <rich.tijerina@gmail.com>
2017-03-31 15:06:28 -0600

    Update KICK.pl

--- KICK.pl
+++ KICK.pl
@@ -5,7 +5,7 @@ use Term::ANSIColor;
 # KICK - print pid
 my (@active) = @ARGV;
 foreach $minion (@active) {
-  my $file = "$minion/$minion".'_PID';
+  my $file = "$minion/PID";
   open(my $pfh, '<', $file);
   my $content = readline $pfh; chomp $content; close $pfh;
   print color('magenta'); print "$minion "; print color('bold yellow'); print "$_\n";
